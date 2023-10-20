de4709c7be3e288ee63ef4ed2252a1b39361f01e
LinYB1 <yubin.lin.2022@scis.smu.edu.sg>
2023-10-20 18:57:55 +0800

    Added code to make it dynamic

---
+++ pages/MySQL/GardenFilter.php
@@ -0,0 +1,40 @@
+<?php
+
+spl_autoload_register(
+    function ($class){
+        require_once  "$class.php";
+    }
+);
+
+$key = $_GET['key'];
+
+$regions = $_GET['regions'];
+$r = "";
+$regions = explode(",", $regions);
+foreach($regions as $region){
+    $r = $r . " Region = '$region' OR";
+}
+$r = substr($r, 0, -3);
+
+$sql = "select * from garden where gardenName like :key and ($r) order by gardenName"; 
+
+$connMgr = new ConnectionManager();
+$pdo = $connMgr->getConnection();
+
+$stmt = $pdo->prepare($sql);
+$in = '%'.$key.'%';
+$stmt->bindParam(':key', $in, PDO::PARAM_STR);
+$stmt->setFetchMode(PDO::FETCH_ASSOC);
+$stmt->execute();
+$result = Array();
+$result['garden'] = [];
+
+while($row = $stmt->fetch()) {
+    $result['garden'][] = array('gardenID' => $row["gardenID"], 'gardenName' => $row["gardenName"], 'Latitude' => $row["latitude"], 'Longitude' => $row["longitude"], 'Region' => $row["region"]);
+}
+
+$stmt = null;
+$pdo = null;
+echo json_encode($result);
+
+?>
