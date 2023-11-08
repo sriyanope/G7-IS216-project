4bb5e5c634b77441cc67dac5ee58d339a0e150c1
sriyanope <sriyamnannapaneni@gmail.com>
2023-11-08 12:43:28 +0800

    Changed pic for loader and event location stuff
    same as above

--- pages/EventLocation.php
+++ pages/EventLocation.php
@@ -51,6 +51,70 @@
             background-color: #547D2E;
         }
 
+        .form-control {
+              outline: 0 !important;
+              border-color: initial;
+              box-shadow: none;
+                    }
+
+          .filter-container {
+            background-color: #f6f8e0;
+            border-radius: 10px;
+            padding: 20px;
+            box-shadow: 0 10px 20px rgba(0, 0, 0, 0.2);
+            color: #547D2E;
+            text-align: center;
+            max-width: 300px;
+            margin: 0, auto;
+            display: flex; 
+            flex-direction: column; 
+            justify-content: center; 
+            align-items: center; 
+          }
+
+          .form-check-input {
+            display: none;
+          }
+
+          .form-check-label {
+            display: inline-block;
+            font-weight: 600;
+            font-size: 18px;
+            cursor: pointer;
+            margin: 10px;
+            position: relative;
+            transition: color 0.3s ease, transform 0.3s ease;
+          }
+
+          .form-check-label::before {
+            content: "\2713";
+            position: absolute;
+            left: -30px;
+            opacity: 0;
+            transform: scale(0.5);
+            transition: opacity 0.3s ease, transform 0.3s ease;
+          }
+
+          .form-check-input:checked + .form-check-label::before {
+            opacity: 1;
+            transform: scale(1);
+          }
+
+          .form-check-label:hover {
+            color: #B7CF9B;
+            transform: scale(1.1);
+          }
+          
+          .filterHead {
+          font-weight: 600;
+          font-size: 12px; 
+          color: black;
+          margin-bottom: 10px;
+          text-align: center; 
+          text-transform: uppercase; 
+          letter-spacing: 2px; 
+          }
+
         .notification {
           position: fixed;
           bottom: 0;
@@ -217,38 +281,35 @@
 
         <!-- filter -->
         <div class="row">
-          <div class="col-1"></div>
-          <div class="col-2">Filter By Region:</div>
+          <div class="col-2 filterHead mt-4">Filter By Region:</div>
           <div class="col-3" id="resultCount"></div>
         </div>
 
         <div class="row">
-        <div class="col-1"></div>
-          <div class="col-2">
-            <div class="bg-light">
+        <div class="col-4">
+            <div class="bg-light filter-container" id="region-checkbox">
               <div class="form-check m-3">
-                <input class="form-check-input" type="checkbox" value="north" onclick="filter(this.value)">
-                <label class="form-check-label">North</label>
+                <input class="form-check-input" type="checkbox" value="north" onclick="filter(this.value)" id="north-checkbox">
+                <label class="form-check-label" for="north-checkbox">North</label>
               </div>
               <div class="form-check m-3">
-                <input class="form-check-input" type="checkbox" value="north-east" onclick="filter(this.value)">
-                <label class="form-check-label">North-East</label>
+                <input class="form-check-input" type="checkbox" value="north-east" onclick="filter(this.value)" id="north-east-checkbox">
+                <label class="form-check-label" for="north-east-checkbox">North-East</label>
               </div>
               <div class="form-check m-3">
-                <input class="form-check-input" type="checkbox" value="central" onclick="filter(this.value)">
-                <label class="form-check-label">Central</label>
+                <input class="form-check-input" type="checkbox" value="central" onclick="filter(this.value)" id="central-checkbox">
+                <label class="form-check-label" for="central-checkbox">Central</label>
               </div>
               <div class="form-check m-3">
-                <input class="form-check-input" type="checkbox" value="east" onclick="filter(this.value)">
-                <label class="form-check-label">East</label>
+                <input class="form-check-input" type="checkbox" value="east" onclick="filter(this.value)" id="east-checkbox">
+                <label class="form-check-label" for="east-checkbox">East</label>
               </div>
               <div class="form-check m-3">
-                <input class="form-check-input" type="checkbox" value="west" onclick="filter(this.value)">
-                <label class="form-check-label">West</label>
+                <input class="form-check-input" type="checkbox" value="west" onclick="filter(this.value)" id="west-checkbox">
+                <label class="form-check-label" for="west-checkbox">West</label>
               </div>
             </div>
-
-          </div>
+        </div>
 
           <!-- garden list -->
           <div class="col-4">
